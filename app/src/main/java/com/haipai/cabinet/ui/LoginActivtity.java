ab9cb50633cd91bdeb732bd050c3a5d9fa9e88af
devin <devin.liu@szhipi.com>
2022-04-22 14:01:22 +0800

    first

---
+++ app/src/main/java/com/haipai/cabinet/ui/LoginActivtity.java
@@ -0,0 +1,16 @@
+package com.haipai.cabinet.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.haipai.cabinet.R;
+
+public class LoginActivtity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_activtity);
+    }
+}
